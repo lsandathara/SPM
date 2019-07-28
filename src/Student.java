dd7b7f242f32c31cd8bc83e5f39b13ed2f336918
lsandathara <dmlakchani@gmail.com>
2019-07-28 13:46:02 +0530

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+
+public class Student {
+
+	private String sId;
+	private String sName;
+	public String getsId() {
+		return sId;
+	}
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	
+	
+}
